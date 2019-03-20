62adb0069cb12e259e2d3bc51aa26c81c6acabeb
Leon12345679 <Leon.vladimirov@gmail.com>
2019-03-20 16:16:55 +0300

    Removed redundant Outlet in Slide xib and class

--- Parkovka/Parkovka/Slide.swift
+++ Parkovka/Parkovka/Slide.swift
@@ -12,11 +12,7 @@ class Slide: UIView {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var labelDesc: UITextView!
     @IBOutlet weak var Settings: UIButton!
-    
-    @IBAction func NextVC(_ sender: UIButton) {
-        
-    }
-    
+
     /*
      // Only override draw() if you perform custom drawing.
      // An empty implementation adversely affects performance during animation.
